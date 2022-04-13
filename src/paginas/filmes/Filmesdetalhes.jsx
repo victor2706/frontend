ee33575935e31e45069157ad93cf40588d19e6ba
Victor2706 <Vmanoel1727@gmail.com>
2022-04-12 23:21:59 -0300

    quarto commit

--- src/paginas/filmes/Filmesdetalhes.jsx
+++ src/paginas/filmes/Filmesdetalhes.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
-import { useParams } from 'react-router-dom'
+import {Link ,useParams } from 'react-router-dom'
 import apiFilmes from '../../Serviços/apiFilmes'
+
 
 const Filmesdetalhes = () => {
 
@@ -23,29 +24,41 @@ const Filmesdetalhes = () => {
   }, [])
   return (
     <div>
-      <h1>{filmes.title}</h1>
 
-      <Row>
-        <Col md={4}>
-          <Card>
-            <Card.Img variant='top' src= {"https://image.tmdb.org/t/p/w500/"+ filmes.poster_path} />
-          </Card>
-        </Col>
-        <Col md={8}>
-          <h2>Titulo Original:</h2>
-          <p>{filmes.original_title}</p>
-          <h2>Popularidade:</h2>
-          <p>{filmes.popularity}</p>
-          <h2>Data de lançamento:</h2>
-          <p>{filmes.release_date}</p>
-          <h2>Orçamento:</h2>
-          <p>{filmes.budget}</p>
-          <h2>Sinopse:</h2>
-          <p>{filmes.overview}</p>
-        </Col>
-      </Row>
-    </div>
-  )
+    {!filmes.id && <h1>Carregando... Aguarde!</h1>}
+
+    {filmes.id &&
+        <div>
+            <h1>{filmes.title}</h1>
+
+            <Row>
+                <Col md={4}>
+                    <Card>
+                        <Card.Img variant="top" src={'https://image.tmdb.org/t/p/w500/' + filmes.poster_path} />
+                    </Card>
+                </Col>
+                <Col md={8}>
+                    <p><strong>Título Original: </strong>{filmes.original_title}</p>
+                    <p><strong>Popularidade: </strong>{filmes.popularity}</p>
+                    <p><strong>Data de Lançamento: </strong>{filmes.release_date}</p>
+                    <p><strong>Orçamento: </strong>{filmes.budget}</p>
+                    
+                    <p><strong>Gêneros: </strong>
+                        {filmes.genres.map(item => (
+                            <span>{item.name}, </span>
+                        ))}
+                    </p>
+
+                    <p><strong>Sinopse: </strong>{filmes.overview}</p>
+
+                    <Link className='btn btn-danger' to={-1}>Retornar</Link>
+
+                </Col>
+            </Row>
+        </div>
+    }
+</div>
+)
 }
 
 export default Filmesdetalhes
